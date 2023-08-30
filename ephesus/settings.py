15f8481b4f0608f3e23bed2874b6946d0ee21e2a
Soni <the.original.soni@gmail.com>
2023-08-30 17:54:12 +0530

    Add allowed host

--- ephesus/settings.py
+++ ephesus/settings.py
@@ -18,9 +18,9 @@ load_dotenv(override=True)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ephesus-2-8d550607f437.herokuapp.com']
 
 # Application definition
 
