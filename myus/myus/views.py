baf7b49aa3764dccc13eab7d7afcbed3c12f5cf2
Isaac Blanco <nuclearhands2001@gmail.com>
2024-02-17 22:20:21 -0500

    Moving the Coalesce to the Subquery

--- myus/myus/views.py
+++ myus/myus/views.py
@@ -176,14 +176,17 @@ def leaderboard(request, id):
 
     # for the sake of simplicity, assume teams won't end up with two correct guesses for a puzzle
     teams = hunt.teams.annotate(
-        score=Subquery(
-            Guess.objects.filter(
-                team=OuterRef("pk"),
-                correct=True,
-            )
-            .values("puzzle__points")
-            .annotate(score=Coalesce(Sum('puzzle__points'), 0))
-            .values("score")
+        score=Coalesce(
+            Subquery(
+                Guess.objects.filter(
+                    team=OuterRef("pk"),
+                    correct=True,
+                )
+                .values("team")
+                .annotate(score=Sum("puzzle__points"))
+                .values("score")
+            ),
+            0,
         ),
         solve_count=Count("guesses", filter=Q(guesses__correct=True)),
         last_solve=Subquery(
@@ -420,9 +423,11 @@ def my_team(request, id):
             "error": error,
             "create_team_form": create_team_form,
             "invite_member_form": invite_member_form,
-            "inviting_teams": Team.objects.filter(hunt=hunt, invited_members=user)
-            if user.is_authenticated
-            else [],
+            "inviting_teams": (
+                Team.objects.filter(hunt=hunt, invited_members=user)
+                if user.is_authenticated
+                else []
+            ),
             "is_organizer": not user.is_anonymous
             and hunt.organizers.filter(id=user.id).exists(),
         },
